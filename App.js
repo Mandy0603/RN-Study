99616cfbd2a6b0303ef40773e9cd0dffd96cca25
Mandy Ma <yandim0317@gmail.com>
2019-06-19 08:57:35 +1000

    Added image.

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { StyleSheet, View } from "react-native";
 
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
+import placeImage from "./src/assets/Melbourne.jpg";
 
 export default class App extends Component {
   state = { places: [] };
@@ -18,16 +19,23 @@ export default class App extends Component {
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random(),
+          name: placeName,
+          image: {
+            uri:
+              "https://www.mantrahotels.com/Portals/0/GalleryImages/System/Locations/Australia/Victoria/Melbourne&Surrounds/Melbourne/Melbourne.t76962.jpg"
+          }
+        })
       };
     });
   };
 
-  placeDeletedHandler = index => {
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       };
     });
@@ -37,7 +45,10 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} />
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
       </View>
     );
   }
--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,23 +1,38 @@
 import React from "react";
 import { View, TextInput, Button, StyleSheet } from "react-native";
 
-const placeInput = props => {
-  return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.placeInput}
-        value={props.placeName}
-        placeholder="an awesome string"
-        onChangeText={props.placeNameChangedHandler}
-      />
-      <Button
-        style={styles.placeButton}
-        title="Add"
-        onPress={props.placeSubmitHandler}
-      />
-    </View>
-  );
-};
+class placeInput extends React.Component {
+  state = { placeName: "" };
+
+  placeNameChangedHandler = val => {
+    this.setState({ placeName: val });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+    this.props.onPlaceAdded(this.state.placeName);
+  };
+
+  render() {
+    return (
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.placeInput}
+          value={this.state.placeName}
+          placeholder="an awesome place"
+          onChangeText={this.placeNameChangedHandler}
+        />
+        <Button
+          style={styles.placeButton}
+          title="Add"
+          onPress={this.placeSubmitHandler}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   inputContainer: {
